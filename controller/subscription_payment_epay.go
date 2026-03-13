58eb1b04413dda28285a78ab8d2d9dfff269814b
shiftshen <shift_shen@163.com>
2026-03-13 23:32:14 +0700

    Fix payment deploy compile error

--- controller/subscription_payment_epay.go
+++ controller/subscription_payment_epay.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/QuantumNous/new-api/common"
+	"github.com/QuantumNous/new-api/logger"
 	"github.com/QuantumNous/new-api/model"
 	"github.com/QuantumNous/new-api/service"
 	"github.com/QuantumNous/new-api/setting/operation_setting"
